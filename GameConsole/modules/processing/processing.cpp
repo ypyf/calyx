d8d551e287b5c686f70f78ccff17f877c19b5ec4
ypyf <t34@qq.com>
2014-10-06 02:59:25 +0800

    Add .pdb of lualib

--- GameConsole/modules/processing/processing.cpp
+++ GameConsole/modules/processing/processing.cpp
@@ -1,14 +1,19 @@
-// calyx.processing模块实现了Processing语言的部分API
+// calyx.processing模块提供了Processing语言的部分API
 
 #include "processing.h"
 #include "../../d3dUtil.h"
 
-#define CALYX_IMAGE_MODE_CORNER	0
+#define CALYX_IMAGE_MODE_CORNER 0
 #define CALYX_IMAGE_MODE_CENTER 1
+
+struct PState
+{
+    int image_mode;
+};
 
 struct ImageData
 {
-	IDirect3DTexture9* tex;
+	IDirect3DTexture9 *tex;
 	D3DXIMAGE_INFO info;
 	void GC()
 	{
@@ -16,17 +21,22 @@ struct ImageData
 	}
 };
 
-int imageMode = CALYX_IMAGE_MODE_CORNER;
+static PState ps;
+
+static void init_pstate()
+{
+    ps.image_mode = CALYX_IMAGE_MODE_CORNER;
+}
 
 static int l_imageMode(lua_State *L)
 {
 	int args = 1;
 	int mode = (int)luaL_optnumber(L, args++, 0);
-	imageMode = mode;
+	ps.image_mode = mode;
 	return 0;
 }
 
-static const struct luaL_reg processinglib[] = 
+static const struct luaL_reg processinglib[] =
 {
 	//{"text", l_text},
 	//{"scale", scale},
@@ -41,6 +51,7 @@ static const struct luaL_reg processinglib[] =
 
 extern "C" int luaopen_calyx_processing(lua_State *L)
 {
+    init_pstate();
 	luaL_openlib(L, "processing", processinglib, 0);
 	return 1;
 }
